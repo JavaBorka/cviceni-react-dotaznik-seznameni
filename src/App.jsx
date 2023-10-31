5ba83a91a7a129bfc2e7c60d3d78e93d167f7f80
JavaBorka <borka34@gmail.com>
2023-10-31 15:30:46 +0100

    Import zadania

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Question from '../src/components/Question'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Question text="Kdo jinému jámu kopá, sám do ní padá." />
+      </div>
+    </>
+  )
+}
+
+export default App
